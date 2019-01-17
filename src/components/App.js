dc8fe61aa2d6f0a880fc8133eca2c54a006e5e31
OussamaAlouat <al317257@uji.es>
2019-01-17 07:45:38 +0100

    Used function (functional component) instead of class (class component) on App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
-class App extends React.Component {
-    render() {
-        return <div>App</div>
-    };
-}
+const App = () => {
+    return <div>App</div>
+};
 
 export default App;
