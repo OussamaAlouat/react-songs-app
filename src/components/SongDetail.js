066028c89b044379c9413b0fa083110bafc5fb80
OussamaAlouat <al317257@uji.es>
2019-01-18 18:16:11 +0100

    Show's the song when it selected

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
-const SongDetail = (props) => {
-    console.log(props)
+const SongDetail = ({song}) => {
 
+    if (!song) {
+        return <div>Not Song selected</div>
+    }
     return (
         <div>
-            Song detail
+            <h3>Details for: </h3>
+            <p>
+                Title: {song.title}
+                <br/>
+                Duration: {song.duration}
+            </p>
+
+
         </div>
     );
 };
 
-const mapStateToProps =(state) => {
-  return {song: state.selectedSong}
+const mapStateToProps = state => {
+    return {song: state.selectedSong}
 };
 
 export default connect(mapStateToProps)(SongDetail);
